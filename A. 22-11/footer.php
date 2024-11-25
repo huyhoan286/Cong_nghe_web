24bfc1f66e56235f9f8a36351b3b326f3de204d2
huyhoan286 <165401952+huyhoan286@users.noreply.github.com>
2024-11-25 18:27:27 +0700

    Delete A. 22-11 directory

--- A. 22-11/footer.php
+++
@@ -1,9 +0,0 @@
-<!-- Thêm đường kẻ ngang trước footer nếu muốn -->
-<hr style="border: 1px solid #ddd; margin: 20px 0;">
-<div class="clearfix">
-    <div class="hint-text" style="text-align: center; font-weight: bold; font-size: 20px;">TLU'S MUSIC GARDEN</div>
-</div>
-
-</body>
-
-</html>
